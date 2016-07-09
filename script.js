c0f4359004254991c0fec6277ea3bfb6729333bd
Sewag <sintayehu244@yahoo.com>
2016-07-09 17:07:04 +0000

    commiting my code

---
+++ script.js
@@ -0,0 +1,13 @@
+<script>
+var numvic=prompt("How many victims do we have?");
+for(var i=0;i<numvic;i++)
+{
+ var vicname=prompt("what is the name of the victom number",i);
+ var arrname.push=vicrec;
+ var vicphone=prompt("what is the victims phone number?")
+ var arrphone.push=vicphone;
+ var address=prompt("what is the victims st address?");
+ var vicaddress.push=address;
+    
+}
+</script>
