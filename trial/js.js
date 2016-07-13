e3a73d889c9130061857da3d06b88b776bddb42e
Sewag <sintayehu244@yahoo.com>
2016-07-13 14:46:58 +0000

    update my code

--- trial/js.js
+++ trial/js.js
@@ -1,3 +1,13 @@
+var vicRec=Array(),vicPhone=Array(),vicStreet=Array(),street,phone;
+var vicNum=prompt("how many victims do u want to register");
+for(var i=0;i<vicNum;i++) {
+    var name=prompt("what is the name of the victim");
+    vicRec.push(name);
+    phone=prompt("what is his phone number");
+    vicPhone.push(phone);
+    street=prompt("what is his address");
+    vicStreet.push(street);
+}
 
 
 
